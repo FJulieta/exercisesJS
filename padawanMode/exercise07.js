8835f2ea74ab43cf2762ec7dc129ddde3fc6500d
Julieta Fader <julieta.fader@mercadolibre.com>
2023-10-31 10:15:01 -0300

    exercise07

---
+++ padawanMode/exercise07.js
@@ -0,0 +1,5 @@
+//Crear una funcion a la cual le pase un array de números y un número que será el resultado de la suma de los dos valores
+// Ejemplo: 
+// sumarDos: ([3,7,8,2], 10) [3,7] suman 10
+// sumarDos: ([4,5,9,1], 10) [9,1] suman 10
+// sumarDos:  ([1,2,3,4], 5) [3,2] suman  5
